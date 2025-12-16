c31d70dd3f2c6c1957265e8b67d1d4af44c7e507
faisalmohammadlocals-cloud <faisal.mohammadlocals@gmail.com>
2025-12-16 12:34:46 -0500

    Add react-icons dependency to package.json

--- app/components/GameCard.jsx
+++ app/components/GameCard.jsx
@@ -1,9 +1,20 @@
+import { SiWindows, SiPlaystation, SiXbox, SiAndroid } from 'react-icons/si';
+
+const platformIcons = {
+  PC: <SiWindows />,
+  PlayStation: <SiPlaystation />,
+  Xbox: <SiXbox />,
+  Mobile: <SiAndroid />,
+};
+
 export default function GameCard({ game }) {
   return (
     <div className="game-card">
       <div className="game-image">
         <img src={game.image} alt={game.name} />
-        <span className="game-platform">{game.platform}</span>
+        <span className="game-platform" title={game.platform}>
+          {platformIcons[game.platform] || game.platform}
+        </span>
       </div>
       <div className="game-content">
         <h3>{game.name}</h3>
