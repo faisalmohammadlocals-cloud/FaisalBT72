d0917f9b766212477c4b1fdbdf5a6f3b23579a4c
faisalmohammadlocals-cloud <faisal.mohammadlocals@gmail.com>
2025-12-16 12:00:39 -0500

    Remove unused Netlify edge rewrite function and related comments

--- netlify/edge-functions/rewrite.js
+++
@@ -1,8 +0,0 @@
-// Netlify edge rewrite removed as part of removing starter template content.
-// If you still need Geo-based rewrites, re-add an appropriate function here.
-
-export const config = { path: "/edge" };
-
-export default function noop(request) {
-    return request.url;
-}
