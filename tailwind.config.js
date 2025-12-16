7429b9bcb4747dd7c856e64a7366bceb020b1948
faisalmohammadlocals-cloud <faisal.mohammadlocals@gmail.com>
2025-12-16 12:29:56 -0500

    Refactor Tailwind CSS configuration and global styles for improved readability and maintainability

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,27 @@
 export default {
   content: [
     './app/**/*.{js,jsx,ts,tsx}',
+    './app/components/**/*.{js,jsx,ts,tsx}',
     './pages/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
+    './styles/**/*.{css,js}',
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#2bdcd2'
+        primary: '#2bdcd2',
+        'primary-content': '#171717',
+        secondary: '#016968',
       },
       fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui']
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
+      },
+      backgroundImage: {
+        noise: `linear-gradient(
+          to bottom,
+          color-mix(in oklab, rgb(5, 5, 5) 0%, transparent),
+          color-mix(in oklab, rgb(5, 5, 5) 100%, transparent)
+        ), url('/images/noise.png')`
       }
     }
   },
